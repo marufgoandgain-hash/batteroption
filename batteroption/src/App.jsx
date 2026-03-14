591eefeba5e892d6f12b970193f0115fe9bd146b
MD Maruf <maruf60463000@gmail.com>
2026-03-14 16:19:41 +0600

    Add Vercel rewrite config for React Router

--- batteroption/src/App.jsx
+++ batteroption/src/App.jsx
@@ -1,15 +1,30 @@
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
+import React, { useState, useEffect } from "react";
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-function App() {
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Page from "./pages/Page";
 
-  const [user] = useAuthState(auth);
+function App() {
+  const [user] = useAuthState(auth); // Google/Facebook login detection
+  const [localUser, setLocalUser] = useState(null); // username/password login detection
+  const [openVideoPage, setOpenVideoPage] = useState(false);
+
+  // যদি কোন ইউজার login থাকে (username/password বা Google/Facebook)
+  const isLoggedIn = user || localUser;
 
   return (
     <div>
-      {user ? <Profile /> : <Login />}
+      {isLoggedIn ? (
+        openVideoPage ? (
+          <Page />
+        ) : (
+          <Profile openVideo={() => setOpenVideoPage(true)} />
+        )
+      ) : (
+        <Login onLogin={setLocalUser} />
+      )}
     </div>
   );
 }
